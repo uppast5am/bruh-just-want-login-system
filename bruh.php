71b4c65a2c93294884d0ae6de75969dcdc68995e
uppast5am <92594564+uppast5am@users.noreply.github.com>
2021-11-22 17:36:35 -0800

    Update bruh.php

--- bruh.php
+++ bruh.php
@@ -54,3 +54,17 @@ function test_input($data) {
   return $data;
 }
 ?>
+
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
