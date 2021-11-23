69d54aafe1549246ccc7c5ee003c358653ff3b2b
uppast5am <92594564+uppast5am@users.noreply.github.com>
2021-11-22 17:52:52 -0800

    Update bruh.php

--- bruh.php
+++ bruh.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = test_input($_POST["name"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
+      $nameErr = "enter your name please";
     }
   }
   
